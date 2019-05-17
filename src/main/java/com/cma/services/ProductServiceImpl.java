980ea6678c38b6dbe1b5ce3ff8619e7007d80da3
Gourav Paul <>
2019-05-17 10:14:07 +0000

    oracle db added

---
+++ src/main/java/com/cma/services/ProductServiceImpl.java
@@ -0,0 +1,60 @@
+package com.cma.services;
+
+import com.cma.commands.ProductForm;
+import com.cma.converters.ProductFormToProduct;
+import com.cma.domain.Product;
+import com.cma.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by jt on 1/10/17.
+ */
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    private ProductRepository productRepository;
+    private ProductFormToProduct productFormToProduct;
+
+    @Autowired
+    public ProductServiceImpl(ProductRepository productRepository, ProductFormToProduct productFormToProduct) {
+        this.productRepository = productRepository;
+        this.productFormToProduct = productFormToProduct;
+    }
+
+
+    @Override
+    public List<Product> listAll() {
+        List<Product> products = new ArrayList<>();
+        productRepository.findAll().forEach(products::add); //fun with Java 8
+        return products;
+    }
+
+    @Override
+    public Product getById(Long id) {
+        return productRepository.findOne(id);
+    }
+
+    @Override
+    public Product saveOrUpdate(Product product) {
+        productRepository.save(product);
+        return product;
+    }
+
+    @Override
+    public void delete(Long id) {
+        productRepository.delete(id);
+
+    }
+
+    @Override
+    public Product saveOrUpdateProductForm(ProductForm productForm) {
+        Product savedProduct = saveOrUpdate(productFormToProduct.convert(productForm));
+
+        System.out.println("Saved Product Id: " + savedProduct.getId());
+        return savedProduct;
+    }
+}
