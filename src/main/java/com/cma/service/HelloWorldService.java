c6516bfbf3ff38491166a9afd8cfc9fb25b6b887
Gourav Paul <>
2019-06-13 18:14:57 +0000

    msg

--- src/main/java/com/cma/service/HelloWorldService.java
+++ src/main/java/com/cma/service/HelloWorldService.java
@@ -1,21 +1,61 @@
-package com.unw.service;
-
-import org.springframework.stereotype.Component;
+package com.cma.service;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
+import javax.inject.Inject;
+import java.util.List;
+import java.util.Date;
+import javax.validation.Valid;
+
+//import com.cma.bean.CustomerBean;
+//import com.cma.model.Customer;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
- * @author UNGERW
+ * @author Gourav
  */
-@Component
-@Path("/spring-docker/")
+
+@RestController
+@RequestMapping(value = "/customer")
 public class HelloWorldService {
 
-    @GET
-    @Path("/hello")
-    public Response test() {
-        return Response.status(200).entity("Western Union Page , - welcome Date: 09.05.2019").build();
-    }
+//	private CustomerBean customerBean;
+
+    	@Inject
+	public  HelloWorldService() {
+//		this.customerBean = customerBean;
+	}
+/*
+	@RequestMapping(value = "/save", method = RequestMethod.GET)
+	public Response saveCustomer(@Valid @RequestParam(value = "name", required = true) final String name, 
+		@Valid @RequestParam(value = "email", required = true) final String email) {
+		this.customerBean.saveCustomer(name, email);
+	
+		return Response.status(200).entity("Customer saved !!").build();
+	}
+
+	@RequestMapping(value = "/name", method = RequestMethod.GET)
+	public Response getUserNameByEmail(@RequestParam(value = "email", required = true) final String email) {
+		Customer customer = this.customerBean.getCustomerByEmail(email);
+		
+		if (customer != null) {
+			return Response.status(200).entity("Customer name : " + customer.getName()).build();
+		} else {
+			return Response.status(200).entity("No user found !!").build();
+		}
+	}
+*/
+	@RequestMapping(value = "/checkapi", method = RequestMethod.GET)
+	public Response test() {
+		return Response.status(200).entity("Today's Demo , - welcome Date: " + new Date()).build();
+	}
 }
+
